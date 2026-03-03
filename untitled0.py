76af411487267d656a7fed39ab8b7e2499b1e1f3
chenaoyu8-design <chenaoyu8@gmail.com>
2026-03-04 00:50:15 +0800

    change the name of this file
    change to leon<111

---
+++ untitled0.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  4 00:40:39 2026
+
+@author: cheny
+"""
+
+name = input("Enter your name: ")
+print(f"Hello, {name}!")
+print("Welcome to the School of Computing Science at UofG.")
